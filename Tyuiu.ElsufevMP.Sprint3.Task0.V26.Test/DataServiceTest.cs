bca80263aa6426107b5c237bb4d33bdc2d860914
Tyuiu.ElsufevMP@outlook.com <mihal.els.32@gmail.com>
2023-10-20 14:02:00 +0500

    Add project files.

---
+++ Tyuiu.ElsufevMP.Sprint3.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ElsufevMP.Sprint3.Task0.V26.Lib;
+
+namespace Tyuiu.ElsufevMP.Sprint3.Task0.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheck()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 9;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = -15.642;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
